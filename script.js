b07a465923de287751945e21df2c03f3bc446b71
AugustoAleman <100232768+AugustoAleman@users.noreply.github.com>
2022-03-01 23:43:56 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-
-function procesarFormulario(){
-  var name = document.getElementById("nombre").value
-  var surname = document.getElementById("apellido").value
-  document.getElementById("salida").innerHTML =
-  "Gracias "+ name +", pronto me pondre en contacto.";
-}
